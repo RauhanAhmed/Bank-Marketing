195bdc58402a6ee95e31e2af02444b5503b63c00
Rauhan Ahmed Siddiqui <rauhaan.siddiqui@gmail.com>
2024-03-15 01:00:59 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import psycopg2
 import pandas as pd
 from dotenv import load_dotenv
 from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
 from src.utils.logging import logger
 from src.utils.exceptions import CustomException
 
@@ -12,6 +13,8 @@ load_dotenv("secrets.env")
 class DataIngestionConfig:
     artifactsDir: str = os.path.join(os.getcwd(), "artifacts")
     dataPath: str = os.path.join("artifacts", "data.csv")
+    trainDataPath: str = os.path.join("artifacts", "trainData.csv")
+    testDataPath: str = os.path.join("artifacts", "testData.csv")
 
 class DataIngestion:
     def __init__(self) -> None:
@@ -19,7 +22,7 @@ class DataIngestion:
         self.dataIngestionConfig = DataIngestionConfig()
         os.system("curl --create-dirs -o $HOME/.postgresql/root.crt 'https://cockroachlabs.cloud/clusters/e5210205-9ccd-49c3-ae85-dae85bf87ac0/cert'")
 
-    def loadData(self) -> None:
+    def extractData(self) -> None:
         try:
             logger.info("executing loadData function")
             password = os.getenv("DB_PASSWORD")
@@ -37,13 +40,25 @@ class DataIngestion:
             logger.info("saving data to artifacts")
             os.makedirs(self.dataIngestionConfig.artifactsDir, exist_ok = True)
             data.to_csv(self.dataIngestionConfig.dataPath, index = False)
-
-            logger.info(">>> DATA INGESTION COMPLETE <<<")
         
         except Exception as e:
             logger.error(CustomException(e))
             print(CustomException(e))
 
-if  __name__ == "__main__":
-    dataIngestion = DataIngestion()
-    dataIngestion.loadData()
+    def splitData(self) -> None:
+        try:
+            data = pd.read_csv(self.dataIngestionConfig.dataPath)
+
+            logger.info("splitting the dataset")
+            X, y = data.drop("y", axis = 1), data["y"]
+            x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+            data = pd.concat([x_train, y_train], axis = 1)
+            testData = pd.concat([x_test, y_test], axis = 1)
+
+            logger.info("saving the train and test data")
+            data.to_csv(self.dataIngestionConfig.trainDataPath, index = False)
+            testData.to_csv(self.dataIngestionConfig.testDataPath, index = False)
+        
+        except Exception as e:
+            logger.error(CustomException(e))
+            print(CustomException(e))
