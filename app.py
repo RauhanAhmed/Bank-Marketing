92092843727aa96d631596f0223dec6570f0c006
Rauhan Ahmed Siddiqui <rauhaan.siddiqui@gmail.com>
2024-03-24 00:45:57 +0530

    Dockerfile

--- app.py
+++ app.py
@@ -65,9 +65,9 @@ def buildInterface():
 
     put_image("https://www.pnbmetlife.com/content/dam/pnb-metlife/images/articles/savings/five-interesting-facts.jpg")
     if result == 0:
-        put_text("/t/tMODEL PREDICTION : The person is not likely to buy a term deposit.")
+        put_text("\n\nMODEL PREDICTION : The person is not likely to buy a term deposit.")
     else:
-        put_text("/t/tMODEL PREDICTION : The person is expected to buy the term deposit.")
+        put_text("\n\nMODEL PREDICTION : The person is expected to buy the term deposit.")
 
 if __name__ == "__main__":
     pywebio.platform.flask.start_server(buildInterface, port = 80, host = "0.0.0.0")
